01e59bffb75bf58bcbd3575d045e0079c2c4b299
Muhammad Isa <meysa110786@gmail.com>
2025-09-19 12:30:51 +0500

    adding new files

---
+++ indexing.py
@@ -0,0 +1,9 @@
+str1 ="rahmat ali khan"
+str2 = "john"
+str3 = "ali sha"
+ch = str1[12]
+print(ch)
+ch = str2[3]
+print(ch)
+ch = str3[2]
+print(ch)
---
+++ strings.py
@@ -0,0 +1,13 @@
+#basic operations
+#concatenation and lenth
+str1 = "my"
+str2 ="college"
+
+len1 =len(str1)
+print(len1)
+len2 =len(str2)
+print(len2)
+
+str3 =str1 + "    " + str2 
+print(str3)
+print (len(str3))
